6039be2b349beae93418ab5abe8490e5fc7d96d3
Dev8391 <8494527+fvreede@users.noreply.github.com>
2021-11-28 23:03:45 +0100

    Add the first object

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-const message = ['']
+// Program generates random text on quotes, mostly fake quotes mashed up with random names.
+
+const msgObject = {
+    quote: ['Be yourself; everyone else is already taken.',
+            'So many books, so little time.',
+            'A room without books is like a body without a soul.',
+            'Two things are infinite: the universe and human stupidity; and I\'m not sure about the universe.',
+            'Be the change that you wish to see in the world.'],
+
+    author: ['Oscar Wilde',
+            'Frank Zappa',
+            'Marilyn Monroe',
+            'Albert Einstein',
+            'Mahatma Gandhi'],
+
+    yearOfDeath: [1900, 1993, 1962, 1955, 1948],
+    
+};
